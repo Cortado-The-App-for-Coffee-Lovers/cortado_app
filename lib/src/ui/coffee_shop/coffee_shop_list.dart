72753770ebb183c7fd645cf48b7391ac9ed68eaa
sam-parks <parkssa123@gmail.com>
2020-04-05 13:49:34 -0500

    Implemented sliver scroll view for coffee shops

---
+++ lib/src/ui/coffee_shop/coffee_shop_list.dart
@@ -0,0 +1,47 @@
+import 'package:cortado_app/src/data/coffee_shop.dart';
+import 'package:cortado_app/src/data/user.dart';
+import 'package:cortado_app/src/ui/widgets/coffee_shop_tile.dart';
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+
+import '../style.dart';
+
+class CoffeeShopsList extends StatefulWidget {
+  CoffeeShopsList({Key key, this.coffeeShops, this.user}) : super(key: key);
+  final List<CoffeeShop> coffeeShops;
+  final User user;
+
+  @override
+  _CoffeeShopsListState createState() => _CoffeeShopsListState();
+}
+
+class _CoffeeShopsListState extends State<CoffeeShopsList> {
+  List<CoffeeShop> _coffeeShops;
+
+  @override
+  void initState() {
+    super.initState();
+    _coffeeShops = widget.coffeeShops;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+        delegate: SliverChildBuilderDelegate(
+      (BuildContext context, int index) {
+        final int itemIndex = index ~/ 2;
+        if (index.isEven) {
+          return CoffeeShopTile(
+              coffeeShop: _coffeeShops[itemIndex], user: widget.user);
+        }
+        return Container(
+          margin: EdgeInsets.symmetric(horizontal: 16.0),
+          color: AppColors.dark,
+          height: 2.0,
+          width: SizeConfig.safeBlockHorizontal * .9,
+        );
+      },
+      childCount: (_coffeeShops.length * 2) - 1,
+    ));
+  }
+}
--- lib/src/ui/coffee_shop/coffee_shop_list_page.dart
+++ lib/src/ui/coffee_shop/coffee_shop_list_page.dart
@@ -1,13 +1,14 @@
 import 'package:cortado_app/src/bloc/coffee_shop/bloc.dart';
 import 'package:cortado_app/src/data/coffee_shop.dart';
 import 'package:cortado_app/src/data/user.dart';
+import 'package:cortado_app/src/ui/coffee_shop/coffee_shop_list.dart';
 import 'package:cortado_app/src/ui/drawer/drawer_home_page.dart';
-import 'package:cortado_app/src/ui/widgets/coffee_shop_tile.dart';
+import 'package:cortado_app/src/ui/widgets/app_bar_with_pic.dart';
 import 'package:cortado_app/src/ui/widgets/cortado_search_bar.dart';
 import 'package:cortado_app/src/ui/widgets/latte_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gradient_app_bar/gradient_app_bar.dart';
+
 import 'package:provider/provider.dart';
 
 import '../style.dart';
@@ -29,6 +30,7 @@ class _CoffeeShopsListPageState extends State<CoffeeShopsListPage> {
   void initState() {
     super.initState();
     _coffeeShopsBloc = BlocProvider.of<CoffeeShopsBloc>(context);
+    _coffeeShopsBloc.add(GetCoffeeShops());
   }
 
   @override
@@ -36,95 +38,37 @@ class _CoffeeShopsListPageState extends State<CoffeeShopsListPage> {
     user = Provider.of<UserModel>(context).user;
     return Scaffold(
       backgroundColor: AppColors.light,
-      appBar: GradientAppBar(
-        gradient: LinearGradient(
-            stops: [.6, .5],
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-            colors: [AppColors.light, AppColors.dark]),
-        brightness: Brightness.dark,
-        elevation: 0,
-        actions: <Widget>[
-          Container(
-            margin: const EdgeInsets.only(right: 25.0),
-            child: Row(
-              children: <Widget>[
-                user.redemptionsLeft != null
-                    ? Text(user.redemptionsLeft.toString())
-                    : Tab(icon: (Image.asset("assets/images/infinity.png"))),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Tab(
-                    icon: Image.asset("assets/images/icons/coffee.png"),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-        iconTheme: IconThemeData(color: AppColors.dark),
-      ),
       drawer: Theme(
         data: Theme.of(context).copyWith(
-          // Set the transparency here
-          canvasColor: Colors
-              .transparent, //or any other color you want. e.g Colors.blue.withOpacity(0.5)
+          canvasColor: Colors.transparent,
         ),
         child: widget.drawer,
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: SizeConfig.blockSizeVertical * .22,
-            width: SizeConfig.screenWidth,
-            child: Stack(
-              children: <Widget>[
-                Positioned(
-                    bottom: 0,
-                    child: Container(
-                        width: SizeConfig.screenWidth,
-                        child: CortadoSearchBar())),
-                Align(
-                    alignment: Alignment.topCenter,
-                    child: Image.asset("assets/images/coffee_shop.png")),
+      body: BlocBuilder(
+        bloc: _coffeeShopsBloc,
+        builder: (context, state) {
+          if (state is CoffeeShopsLoaded) {
+            List<CoffeeShop> updatedCoffeeShops =
+                _sortAndFilterCoffeeList(state.coffeeShops);
+            return CustomScrollView(
+              slivers: <Widget>[
+                AppBarWithImage(
+                  image: Container(
+                      height: SizeConfig.screenHeight * .2,
+                      child: Image.asset("assets/images/coffee_shop.png")),
+                  actions: coffeeRedemptionWidget(user),
+                  lower: CortadoSearchBar(),
+                ),
+                CoffeeShopsList(
+                  coffeeShops: updatedCoffeeShops,
+                  user: user,
+                ),
               ],
-            ),
-          ),
-          BlocBuilder(
-              bloc: _coffeeShopsBloc,
-              builder: (BuildContext context, state) {
-                if (state is CoffeeShopsLoaded) {
-                  List<CoffeeShop> updatedCoffeeShopList;
-                  updatedCoffeeShopList =
-                      _sortAndFilterCoffeeList(state.coffeeShops);
-                  _coffeeShops = updatedCoffeeShopList;
-                  return Container(
-                    height: SizeConfig.blockSizeVertical * .66,
-                    child: ListView.separated(
-                      itemCount: updatedCoffeeShopList.length,
-                      itemBuilder: (context, index) {
-                        CoffeeShop coffeeShop = updatedCoffeeShopList[index];
+            );
+          }
 
-                        return CoffeeShopTile(
-                            coffeeShop: coffeeShop, user: user);
-                      },
-                      separatorBuilder: (BuildContext context, int index) {
-                        return Container(
-                          margin: EdgeInsets.symmetric(horizontal: 16.0),
-                          color: AppColors.dark,
-                          height: 2.0,
-                          width: SizeConfig.safeBlockHorizontal * .9,
-                        );
-                      },
-                    ),
-                  );
-                }
-                return Container(
-                    padding:
-                        EdgeInsets.only(top: SizeConfig.safeBlockVertical * .3),
-                    child: LatteLoader());
-              }),
-        ],
+          return Center(child: LatteLoader());
+        },
       ),
     );
   }
@@ -140,4 +84,25 @@ class _CoffeeShopsListPageState extends State<CoffeeShopsListPage> {
     }).toList();
     return sortedAndFilterList;
   }
+}
+
+List<Widget> coffeeRedemptionWidget(User user) {
+  return <Widget>[
+    Container(
+      margin: const EdgeInsets.only(right: 25.0),
+      child: Row(
+        children: <Widget>[
+          user.redemptionsLeft != null
+              ? Text(user.redemptionsLeft.toString())
+              : Tab(icon: (Image.asset("assets/images/infinity.png"))),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Tab(
+              icon: Image.asset("assets/images/icons/coffee.png"),
+            ),
+          ),
+        ],
+      ),
+    ),
+  ];
 }
